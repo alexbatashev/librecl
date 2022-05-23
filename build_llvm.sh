5f47f2594041cc0666cb0bb2bbba9409518b5b45
Alexander Batashev <alexbatashev@outlook.com>
2022-05-23 10:41:33 +0300

    Enable github actions

--- build_llvm.sh
+++ build_llvm.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ $CI -eq true ]; then
+  echo "enabling ccache for GitHub Actions"
+  export CACHE_LOC=$GITHUB_WORKSPACE/cache
+fi
 
 cd third_party/llvm-project
 
 mkdir -p build && cd build
 
-cmake -GNinja -DLLVM_ENABLE_PROJECTS="mlir;clang;compiler-rt;libclc;lld" \
+cmake -GNinja -DLLVM_ENABLE_PROJECTS="mlir;clang;lld" \
   -DLLVM_TARGETS_TO_BUILD="host;AMDGPU;NVPTX" \
   -DLLVM_ENABLE_ASSERTIONS=OFF \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DLIBCLC_TARGETS_TO_BUILD="nvptx64--;nvptx64--nvidiacl;amdgcn--;amdgcn--amdhsa" \
+  -DLLVM_CCACHE_MAX_SIZE="8G" \
+  $CACHE_LOC \
   -DLLVM_CCACHE_BUILD=ON ../llvm/
 
 ninja
